0c423510e71a1e70d50fa163d99f3d9cd05de17f
Faldin865 <gpaul@student.42.fr>
2022-12-13 11:52:01 +0100

    1st commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Routes from './src/config/routes';
+
+//On mets les routes dans un composant "Routes"
+const App = () => {
+  return <Routes />;
+};
+
+export default App;
